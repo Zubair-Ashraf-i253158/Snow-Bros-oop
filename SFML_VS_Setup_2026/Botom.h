dbe810a5dd731e189f8d0a5fc8863cfd30daea7f
Abdullah Saqib <i252020@isb.nu.edu.pk>
2026-04-28 20:39:28 +0500

    which enemy in every level doen

--- SFML_VS_Setup_2026/Botom.h
+++ SFML_VS_Setup_2026/Botom.h
@@ -8,8 +8,13 @@ protected:
 
 	   float jumpE;    // jump strength
 	   bool groundE;     // fall speed
-	   int animFrame = 0;
-	   int animTimer = 0;
+	  // int animFrame = 0;
+	  // int animTimer = 0;
+
+
+	   //botom alsoi jump randomly
+	   int jumpT; // ya count kre ga ka kitna time guuzar gaya after jump 
+	   float jumpD;  //kitne time baad jummp krni ha 
 
    public:
 	   Botom() : Botom(0, 0) {}
