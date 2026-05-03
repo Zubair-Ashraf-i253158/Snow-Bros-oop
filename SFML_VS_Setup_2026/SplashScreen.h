7c496e9092d6280c55e6d507666519bdac55d0ab
Zubair Ashraf <i253158@isb.nu.edu.pk>
2026-05-04 01:11:27 +0500

    splash screen

--- SFML_VS_Setup_2026/SplashScreen.h
+++ SFML_VS_Setup_2026/SplashScreen.h
@@ -4,32 +4,23 @@
 class SplashScreen
 {
 private:
-    sf::Texture bgTexture;
-    sf::Sprite bg;
-    sf::Font font;
-    sf::Text title;
     sf::Text pressKey;
     float timer = 0;
-
+	sf::Texture bannerTexture;
+	sf::Sprite banner;
 public:
     SplashScreen()
     {
-        font.loadFromFile("assets/FONT/font.ttf");
-
-        title.setFont(font);
-        title.setString("SNOW BROS");
-        title.setFillColor(sf::Color::Cyan);
-        title.setCharacterSize(60);          // size kam karo
-        title.setPosition(150, 200);         // center mein
-
-        pressKey.setFont(font);
-        pressKey.setString("Press Any Key to Start");
-        pressKey.setCharacterSize(25);
-        pressKey.setFillColor(sf::Color::White);
-        pressKey.setPosition(170, 380);      // theek position
+		bannerTexture.loadFromFile("assets/banner.png");
+		banner.setTexture(bannerTexture);
+        banner.setScale(
+            800.0f / bannerTexture.getSize().x,
+            600.0f / bannerTexture.getSize().y
+        );
+       
     }
 
-    int update(sf::Event& event)  // & lagao - reference
+    int update(sf::Event& event)  //press any key ka lia
     {
         if (event.type == sf::Event::KeyPressed ||
             event.type == sf::Event::MouseButtonPressed)
@@ -39,8 +30,7 @@ public:
 
     void draw(sf::RenderWindow& window)
     {
-        window.clear(sf::Color(20, 20, 80));
-        window.draw(title);
-        window.draw(pressKey);
+		window.draw(banner);
+        
     }
 };
